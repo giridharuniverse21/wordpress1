af25cf3ab1fd130fdfe4965f79add778bd857d94
giridharuniverse21 <97376524+giridharuniverse21@users.noreply.github.com>
2022-03-18 18:07:08 +0530

    Update footer.php

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -36,7 +36,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress live test GIRIDHAR@ NCODEIT', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by WordPress live test NEW DATA GIRIDHAR@ NCODEIT', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
